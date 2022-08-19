e8e46d4cb0182ee7e3b19634e78fcc25d2978944
Serzhan Akhmetov <siaarzh@gmail.com>
2022-08-19 22:05:26 +0600

    End of 010
    - Variables Demo

--- src/lectures/demo/variables/variables.go
+++ src/lectures/demo/variables/variables.go
@@ -3,4 +3,34 @@ package main
 import "fmt"
 
 func main() {
+	var myName = "Jayson"
+	fmt.Println("my name is", myName)
+
+	var name string = "Kathy"
+	fmt.Println("name =", name)
+
+	userName := "admin"
+	fmt.Println("userName =", userName)
+
+	var sum int
+	fmt.Println("The sum is", sum)
+
+	part1, other := 1, 5
+	fmt.Println("part 1 is", part1, "other is", other)
+
+	part2, other := 2, 0
+	fmt.Println("part 2 is", part2, "other is", other)
+
+	sum = part1 + part2
+	fmt.Println("The sum is", sum)
+
+	var (
+		lessonName = "Variables"
+		lessonType = "Demo"
+	)
+	fmt.Println("Lesson Name:", lessonName)
+	fmt.Println("Lesson Type:", lessonType)
+
+	word1, word2, _ := "Hello", "World", "!"
+	fmt.Println(word1, word2)
 }
