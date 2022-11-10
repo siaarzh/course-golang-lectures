67d7a84d1bd2d09a9a33eeb59283a327ed44c5ce
Serzhan Akhmetov <siaarzh@gmail.com>
2022-11-10 15:57:43 +0600

    End of 022

--- src/lectures/demo/loops/loops.go
+++ src/lectures/demo/loops/loops.go
@@ -3,5 +3,28 @@ package main
 import "fmt"
 
 func main() {
+	sum := 0
+	fmt.Println("Sum is", sum)
+	for i := 1; i <= 10; i++ {
+		sum += i
+		fmt.Println("Sum is", sum)
+	}
 
+	for sum > 10 {
+		sum -= 5
+		fmt.Println("Decrement. Sum is", sum)
+	}
+
+	for {
+		sum--
+		if sum%2 == 0 {
+			continue
+		}
+		fmt.Println("While loop. Sum", sum)
+
+		if sum == -3 {
+			break
+		}
+
+	}
 }
