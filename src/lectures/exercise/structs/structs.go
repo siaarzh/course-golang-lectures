ec5b4f8580ef3cdb04967a50dfa989b264c7601b
Serzhan Akhmetov <siaarzh@gmail.com>
2022-11-12 19:42:08 +0600

    End of 027

--- src/lectures/exercise/structs/structs.go
+++ src/lectures/exercise/structs/structs.go
@@ -17,8 +17,47 @@
 
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
+
+// Rectangle
+//* Create a rectangle structure containing its coordinates
+type Rectangle struct {
+	x0, y0, x1, y1 float64
+	area           float64
+	perimeter      float64
+}
+
+// calculateArea
+//* Using functions, calculate the area and perimeter of a rectangle,
+//  - The functions must use the rectangle structure as the function parameter
+func calculateArea(r Rectangle) float64 {
+	return math.Abs(r.x1-r.x0) * math.Abs(r.y1-r.y0)
+}
+
+func calculatePerimeter(r Rectangle) float64 {
+	return 2 * (math.Abs(r.x1-r.x0) + math.Abs(r.y1-r.y0))
+}
+
+func printRect(r Rectangle) {
+	fmt.Println("Area is:", r.area)
+	fmt.Println("Perimeter is:", r.perimeter)
+}
 
 func main() {
-
+	rect := Rectangle{x0: 0.0, x1: 4.0, y0: 0.0, y1: 8.0}
+	rect.area = calculateArea(rect)
+	rect.perimeter = calculatePerimeter(rect)
+	//  - Print the results to the terminal
+	printRect(rect)
+	//* After performing the above requirements, double the size
+	//  of the existing rectangle and repeat the calculations
+	rect.x1 = rect.x1 * 2
+	rect.y1 = rect.y1 * 2
+	rect.area = calculateArea(rect)
+	rect.perimeter = calculatePerimeter(rect)
+	//  - Print the new results to the terminal
+	printRect(rect)
 }
