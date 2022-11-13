8f8438b3ef77ec723951adce42289c3f7702d688
Serzhan Akhmetov <siaarzh@gmail.com>
2022-11-13 21:18:58 +0600

    End of 029

--- src/lectures/demo/arrays/arrays.go
+++ src/lectures/demo/arrays/arrays.go
@@ -2,6 +2,34 @@ package main
 
 import "fmt"
 
-func main() {
+type Room struct {
+	name    string
+	cleaned bool
+}
 
+func checkCleanRoom(rooms [4]Room) {
+	for i := 0; i < len(rooms); i++ {
+		room := rooms[i]
+		if room.cleaned {
+			fmt.Println(room.name, "is clean")
+		} else {
+			fmt.Println(room.name, "is dirty")
+		}
+	}
+}
+func main() {
+	rooms := [...]Room{
+		{name: "Office"},
+		{name: "Warehouse"},
+		{name: "Reception"},
+		{name: "Ops"},
+	}
+
+	checkCleanRoom(rooms)
+
+	fmt.Println("Performing cleaning...")
+	rooms[0].cleaned = true
+	rooms[3].cleaned = true
+
+	checkCleanRoom(rooms)
 }
